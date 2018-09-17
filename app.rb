aa5ce93824e6c0a187cbd22d6cac09f5923a8fde
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-17 16:22:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,26 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    # rev_name = params[:name].reverse
+    # '#{rev_name}'
+    erb :reversename
+  end
+
+  get '/square/:number' do
+    erb :square
+  end
+
+  get '/say/:number/:phrase' do
+    erb :phrase
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    erb :word
+  end
+
+  get '/:operation/:num1/:num2' do
+    erb :operation
+  end
 
 end
